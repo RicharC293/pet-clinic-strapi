e1d2d87f2c2852525ce84f7b8db9548c9479ed79
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-01-11 23:02:39 -0500

    Update database configuration and generation date in documentation

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,16 @@
-
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "mysql",
-        host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env("DATABASE_HOST", process.env.DATABASE_HOST),
         port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
         database: env("DATABASE_NAME", process.env.DATABASE_NAME),
         username: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
         password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
-        ssl: env.bool("DATABASE_SSL", true),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
-      options: {},
     },
+    debug: false,
   },
 });
