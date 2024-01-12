c92ba68986dabdf7e5b8a877608194d720c2f812
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-01-11 23:10:22 -0500

    Update production database configuration

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,19 @@
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
-    connection: {
-      host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", process.env.DATABASE_HOST),
         port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
         database: env("DATABASE_NAME", process.env.DATABASE_NAME),
         username: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
         password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        ssl: env.bool("DATABASE_SSL", true),
       },
+      options: {},
     },
-    debug: false,
   },
 });
